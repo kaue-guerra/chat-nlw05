366e0252f0985e983632acacf181f7a3dd6b5e9c
kaue-guerra <kaueguerra@gmail.com>
2021-04-22 15:52:29 -0300

    created entities, controllers, repositories and services for Message, User and Settings

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,21 @@
 import { request, response, Router } from "express";
 import { getCustomRepository } from "typeorm";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 import { SettingsRepository } from "./repositories/SettingsRepository";
 
 const routes = Router();
 
 const settingsController = new SettingController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create)
+
+routes.post("/users", usersController.create)
+
+routes.post("/messages", messagesController.create)
+routes.post("/messages/:id", messagesController.showByUser)
 
 export { routes }
